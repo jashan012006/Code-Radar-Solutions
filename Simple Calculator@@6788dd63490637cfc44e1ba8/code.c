652f76a9d7a507e5954f9398966fe6729da70e5d
jashan012006 <jashan0229.becse24@chitkara.edu.in>
2025-01-27 14:34:06 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 int main() {
-    int a;
-    int b;
+    float a;
+    float b;
     char character;
     scanf("%d %d %c",&a,&b,&character);
     switch(character){
