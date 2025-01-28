36681825292ae7526a84c3827fc403a15a12d6a7
jashan012006 <jashan0229.becse24@chitkara.edu.in>
2025-01-28 08:59:44 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -4,7 +4,7 @@ int main(){
     char c;
     scanf("%c",&c);
     if(isalpha(c)){
-        c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U'{
+       if (c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U'){
         printf("Vowel");
     }else{
         printf("Consonant");
